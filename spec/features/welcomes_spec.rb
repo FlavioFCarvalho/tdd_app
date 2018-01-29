e06824aa57d3a0d6e56925d63847d7ef1c29ae42
Flavio Carvalho <flaviocfr@gmail.com>
2018-01-29 21:39:27 -0200

    Test welcome index

--- spec/features/welcomes_spec.rb
+++ spec/features/welcomes_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.feature "Welcomes", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+feature "Welcomes", type: :feature do
+  scenario 'Mostra mensagem de Bem-Vindo' do
+    visit('/')
+    expect(page).to have_content('Bem-Vindo')
+   end
 end
